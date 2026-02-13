57fc30537af241e5ceb27ba68371a2daf3a3063d
Ninorocks04 <antonio.r.accardo@gmail.com>
2026-02-12 19:55:51 -0500

    Added data excel writing

--- Thruster_Data.m
+++ Thruster_Data.m
@@ -1,7 +1,7 @@
 function [tot_Isp, X_ac, theta_ac] = Thruster_Data(u_inputs,x_states,t_states)
 % This function uses simulation data to produces various relavent data
 
-itr = length(x_states);
+[~, itr] = size(x_states);
 % Caclulating the total specific impulse exerted by the cubesat
 dt = t_states(2) - t_states(1);
 
