6c7e539159f2596535803187cd92d9e2ee7ed729
Ninorocks04 <antonio.r.accardo@gmail.com>
2026-03-16 16:27:26 -0400

    adjusted thrust

--- Thruster_Controls.m
+++ Thruster_Controls.m
@@ -32,7 +32,7 @@ R = 0.16;
 R = 1/(ubar^2);
 
 % Calling CubeSat Function
-[A, B, K] = CubeSat_Vacco8T(Xw,Vw,thetaw,ww,dt,R);
+[A, B, K] = CubeSat_12T(Xw,Vw,thetaw,ww,dt,R);
 
 
 % Setting a position deviations (in y)
@@ -75,8 +75,8 @@ dx = 0.01;
 ubar = 25/1000;
 dt = 1e-2;
 
-w = 1/(dx^2);
-R = 1/(ubar^2);
+w = 10; %1/(dx^2);
+R = 1; %1/(ubar^2);
 
 [Aa, Ba, K] = CubeSat_Vacco8T(w,w,w,w,dt,R)
 
